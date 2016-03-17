aa9047ef5baa12ca00a6af053cb8d9c32021396b
chyeh <chyeh@cepave.com>
2016-03-17 14:42:50 +0800

    [OWL-384] Check if the modules in arguments exist
    Now all the commands check the input arguments at the beginning,
    except the `reload` command because of the fe workaround.

--- g/const.go
+++ g/const.go
@@ -3,7 +3,12 @@ package g
 const (
 	LogDir = "./logs"
 
+	// Modules' status
 	ModuleRunning            = 0
 	ModuleExistentNotRunning = 1
 	ModuleNonexistent        = 2
+
+	//Command exit code
+	Command_EX_OK  = 0
+	Command_EX_ERR = 1
 )
